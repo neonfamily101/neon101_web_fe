ea06127cdb3083133e4c42cf31d454339a65f65b
LEE JUN SEO <neonfamily101@gmail.com>
2025-06-19 17:14:21 +0900

    Update : 리다이렉션 수정

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,20 +1,3 @@
-// import { NextResponse } from 'next/server';
-// import type { NextRequest } from 'next/server';
-
-// export function middleware(request: NextRequest) {
-//   const host = request.headers.get('host') || '';
-//   const url = request.nextUrl;
-
-//   // www가 없는 경우 www로 리디렉션
-//   if (host === 'neon101.ai') {
-//     return NextResponse.redirect(`https://www.neon101.ai${url.pathname}`, 301);
-//   }
-
-//   return NextResponse.next();
-// }
-
-
-
 // src/middleware.ts
 import { NextResponse } from 'next/server';
 import type { NextRequest } from 'next/server';
@@ -31,7 +14,7 @@ export function middleware(request: NextRequest) {
   return NextResponse.next();
 }
 
-// 👇 matcher가 없으면 middleware가 작동하지 않을 수 있음
 export const config = {
-  matcher: ['/', '/((?!_next|favicon.ico|images|videos).*)'],
+  matcher: ['/((?!_next|.*\\..*).*)'],
 };
+
