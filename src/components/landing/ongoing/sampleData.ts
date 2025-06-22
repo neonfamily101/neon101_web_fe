aa76ff18fdcbbaa91470f1731c8ec9a5fb15822e
LEE JUN SEO <neonfamily101@gmail.com>
2025-06-22 16:45:36 +0900

    Update : footer webGL 삭제

--- src/components/landing/ongoing/sampleData.ts
+++ src/components/landing/ongoing/sampleData.ts
@@ -4,49 +4,49 @@ export const sampleData = [
     id: '1',
     title: 'Financial Cloud VAN',
     description: 'NEON101은 금융클라우드 VAN사와 결제 시스템과 관련된 공동의 프로젝트를 진행 중입니다.',
-    videoSrc: 'https://storage.googleapis.com/neon101-videos/clientsVideos/VAN.mp4',
+    videoSrc: 'https://storage.googleapis.com/neon101-videos/clientsVideos/VAN.webm',
     videoSubSrc: 'https://storage.googleapis.com/neon101-videos/clientsVideos/VAN.mp4'
   },
   {
     id: '2',
     title: 'Hyper-Space',
     description: 'NEON101은 Antler + BNI + SparkPlus + VC를 통섭시킨 공간 프로젝트를 진행중입니다.',
-    videoSrc: 'https://storage.googleapis.com/neon101-videos/clientsVideos/hyper_space.mp4',
+    videoSrc: 'https://storage.googleapis.com/neon101-videos/clientsVideos/hyper_space.webm',
     videoSubSrc: 'https://storage.googleapis.com/neon101-videos/clientsVideos/hyper_space.mp4'
   },
   {
     id: '3',
     title: 'C-Commerce',
     description: 'NEON101은 링고글로벌의 AI-TECH 파트너스로서 China-Commerce에 관련된 거대 프로젝트를 진행 중입니다.',
-    videoSrc: 'https://storage.googleapis.com/neon101-videos/clientsVideos/c_commerce.mp4',
+    videoSrc: 'https://storage.googleapis.com/neon101-videos/clientsVideos/c_commerce.webm',
     videoSubSrc: 'https://storage.googleapis.com/neon101-videos/clientsVideos/c_commerce.mp4'
   },
   {
     id: '4',
     title: 'AI-Entertainment',
     description: 'NEON101은 기존의 엔터테인먼트 사업을 AI 기술을 활용하여 COST-REDUCTION과 PROFIT-CREATION 프로젝트를 진행 중입니다. ',
-    videoSrc: 'https://storage.googleapis.com/neon101-videos/clientsVideos/AI_Entertainment.mp4',
+    videoSrc: 'https://storage.googleapis.com/neon101-videos/clientsVideos/AI_Entertainment.webm',
     videoSubSrc: 'https://storage.googleapis.com/neon101-videos/clientsVideos/AI_Entertainment.mp4'
   },
   {
     id: '5',
     title: 'AI-Webtoon',
     description: 'NEON101은 MUTI-MODAL의 다섯 가지의 궁극의 조합으로 채팅앱을 형성된 TEXT DB와 VISUAL 결과물인 웹툰 생성 프로젝트를 진행 중입니다.',
-    videoSrc: 'https://storage.googleapis.com/neon101-videos/clientsVideos/AI_webtoon.mp4',
+    videoSrc: 'https://storage.googleapis.com/neon101-videos/clientsVideos/AI_webtoon.webm',
     videoSubSrc: 'https://storage.googleapis.com/neon101-videos/clientsVideos/AI_webtoon.mp4'
   },
   {
     id: '6',
     title: 'AX Marketing Platform',
     description: 'NEON101은 UNDER 50 회사들을 위한 AX Marketing Platform 구축사업을 진행 중입니다.',
-    videoSrc: 'https://storage.googleapis.com/neon101-videos/clientsVideos/AX_Marketing.mp4',
+    videoSrc: 'https://storage.googleapis.com/neon101-videos/clientsVideos/AX_Marketing.webm',
     videoSubSrc: 'https://storage.googleapis.com/neon101-videos/clientsVideos/AX_Marketing.mp4',
   },
   {
     id: '7',
     title: 'Global MRO',
     description: 'NEON101은 온라인 다이소를 꿈꾸며 글로벌 ERP 시스템과 연동된 MRO 비즈니스 프로젝트를 진행 중입니다.',
-    videoSrc: 'https://storage.googleapis.com/neon101-videos/clientsVideos/Global_MRO.mp4',
+    videoSrc: 'https://storage.googleapis.com/neon101-videos/clientsVideos/Global_MRO.webm',
     videoSubSrc: 'https://storage.googleapis.com/neon101-videos/clientsVideos/Global_MRO.mp4'
   },
 ];
